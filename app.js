b6176cf648158d1dff73aed12d4ba8fe47f2bfbe
S M Khirul Alam <ka.shagor1@gmail.com>
2022-06-06 17:41:55 +0600

    Made Some Core Changes to redirect to work and add the news task there

--- app.js
+++ app.js
@@ -2,29 +2,38 @@ const express = require("express");
 const bodyParser =  require("body-parser");
 
 const app = express();
-var items = ["Buy Food","Eat Food","Cook Food"];
+let items = ["Buy Food","Eat Food","Cook Food"];
+let workItems =  [];
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
-	var today = new Date();
-	var options = {
+	let today = new Date();
+	let options = {
 		weekday: "long",
 		day: "numeric",
 		month: "long"
 	}
-	var cDay = today.toLocaleDateString("en-US",options);
+	let cDay = today.toLocaleDateString("en-US",options);
 
-	res.render("list",{currnetDay:cDay,newListItems: items}); // {value_to_be_repaced:replacing_value} 
+	res.render("list",{listTitle:cDay,newListItems: items}); // {value_to_be_repaced:replacing_value} 
 
 });
-
 app.post("/",(req,res)=>{
-	item = req.body.newtask;
-	items.push(item);
-	res.redirect("/");
-})
+	item =  req.body.newtask;
+	if(req.body.list === "Work"){
+			workItems.push(item);
+			res.redirect("/work");
+	}else{
+		items.push(item);
+		res.redirect("/");
+	}
+});
+app.get("/work",(req,res)=>{
+	res.render("list",{listTitle:"Work",newListItems:workItems});
+});
 app.listen(3000,()=>{
 	console.log("Server Started on Port 3000");
-})
+});
