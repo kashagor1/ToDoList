8763df46184a39c47a2fd02bb6e68293570e8454
S M Khirul Alam <ka.shagor1@gmail.com>
2022-06-06 16:54:31 +0600

    Learned Passing Data to ejs file and using it

--- app.js
+++ app.js
@@ -2,43 +2,29 @@ const express = require("express");
 const bodyParser =  require("body-parser");
 
 const app = express();
+var items = ["Buy Food","Eat Food","Cook Food"];
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
 	var today = new Date();
-	var cDay = '';
-	
-	switch(today.getDay()){
-		case 0:
-			cDay = "Sunday";
-			break;
-		case 1:
-			cDay = "Monday";
-			break;
-		case 2:
-			cDay = "Tuesday";
-			break;
-		case 3:
-			cDay = "Wednesday";
-			break;
-		case 4:
-			cDay = "Thursday";
-			break;
-		case 5:
-			cDay = "Friday";
-			break;
-		case 6:
-			cDay = "Saturday";
-			break;
-		default:
-		console.log("Error current is equal to: "+today.getDay());
+	var options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long"
 	}
+	var cDay = today.toLocaleDateString("en-US",options);
 
-	res.render("list",{currnetDay:cDay}); // {value_to_be_repaced:replacing_value} 
+	res.render("list",{currnetDay:cDay,newListItems: items}); // {value_to_be_repaced:replacing_value} 
 
 });
 
+app.post("/",(req,res)=>{
+	item = req.body.newtask;
+	items.push(item);
+	res.redirect("/");
+})
 app.listen(3000,()=>{
 	console.log("Server Started on Port 3000");
 })
