8dbf18ebc1d75945bb512c0c1d67779129229512
S M Khirul Alam <ka.shagor1@gmail.com>
2022-06-13 21:14:17 +0600

    We hvae connected our localhost app to mongoDB atlas

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 
-mongoose.connect('mongodb://localhost:27017/todolistDBVV', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://todolistSagor:KmDjq6FDinZWPOxN@cluster0.g7yhifc.mongodb.net/todolistDBVV', {useNewUrlParser: true});
 
 const itemSchema = {
 	name : String
